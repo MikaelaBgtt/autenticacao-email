548232bca8338383aa515726c4fe7aa2687d9694
Mikaéla <mikaela.cardoso@fatec.sp.gov.br>
2023-10-03 15:55:46 -0300

    [refactor]: alteração de imports na domain de email

--- notes/src/main/java/com/example/notes/domain/Email.java
+++ notes/src/main/java/com/example/notes/domain/Email.java
@@ -1,30 +1,36 @@
-package main.java.com.example.notes.domain;
+package com.example.notes.domain;
 
 import java.io.Serializable;
 import java.time.LocalDateTime;
 
+import com.example.notes.enums.EmailStatus;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import main.java.com.example.notes.enums.EmailStatus;
+import lombok.Data;
 
 @Data
 @Table(name = "EMAIL")
 @Entity
+
 public class Email implements Serializable {
     private static final long serialVersionID = 1L;
 
     @Id
-    @GeneratedValues(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long emailId;
     private String ownerRef;
     private String emailFrom;
     private String emailTo;
-    private String subject;
+
     @Column(columnDefinition = "TEXT")
     private String text;
-    private LocalDateTime sendDataEmail;
+    private LocalDateTime sendDateEmail;
+    private String subject;
     private EmailStatus statusEmail;
 
 }
