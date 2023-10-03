571e4758ef147eae8985118fe8b44fcad7a4e790
Mikaéla <mikaela.cardoso@fatec.sp.gov.br>
2023-10-03 15:52:01 -0300

    [feat]: adição de imports na EmailRepository

--- notes/src/main/java/com/example/notes/repository/EmailRepository.java
+++ notes/src/main/java/com/example/notes/repository/EmailRepository.java
@@ -1,7 +1,10 @@
-package main.java.com.example.notes.repository;
+package com.example.notes.repository;
 
-import main.java.com.example.notes.domain.Email;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.notes.domain.Email;
 
 public interface EmailRepository extends JpaRepository<Email,Long> {
     
+
 }
