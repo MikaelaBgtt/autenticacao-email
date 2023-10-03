fc4cc46975ae4f54a75f2a016a1072fb00d79b19
Mikaéla <mikaela.cardoso@fatec.sp.gov.br>
2023-10-03 17:24:15 -0300

    [feat]: controller de usuários recebendo a injeção de dependência do email

--- notes/src/main/java/com/example/notes/controllers/UserController.java
+++ notes/src/main/java/com/example/notes/controllers/UserController.java
@@ -8,7 +8,10 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.notes.domain.Email;
 import com.example.notes.domain.User;
+import com.example.notes.dtos.UserDto;
+import com.example.notes.services.EmailService;
 import com.example.notes.services.UserService;
 
 @RestController
@@ -16,21 +19,33 @@ import com.example.notes.services.UserService;
 public class UserController {
 
     private final UserService userService;
+    private final EmailService emailService;
     
     @Autowired
-    public UserController(UserService userService) {
+    public UserController(UserService userService, EmailService emailService) {
         this.userService = userService;
+        this.emailService = emailService;
     }
 
     @PostMapping("/create")
-    public ResponseEntity<String> createUser(@RequestBody String email, String username, String password) {
+    public ResponseEntity<String> createUser(@RequestBody UserDto userDto) {
         try {
-            User createdUser = userService.createUser(username,email,password);
+            User createdUser = userService.createUser(userDto.getUsername(), userDto.getEmail(), userDto.getPassword());
+            Email email = new Email();
+            email.setOwnerRef("Mika");
+            email.setEmailFrom("mibegotti@gmail.com");
+            email.setEmailTo(createdUser.getEmail());
+            email.setSubject("Confirme seu e-mail");
+            email.setText("Clique no link para confirmar: ");
 
+            emailService.sendEmail(email);
             return new ResponseEntity<>("User created with ID: " + createdUser.getId(), HttpStatus.CREATED);
         
         } catch (Exception e) {
             return new ResponseEntity<>("Failed to create user: " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
+    
+    
+    
 }
