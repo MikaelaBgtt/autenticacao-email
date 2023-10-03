36f109b01ff8f5496f95add2c9c08503c839d5ab
Mikaéla <mikaela.cardoso@fatec.sp.gov.br>
2023-10-03 12:31:24 -0300

    [feat]: Email DTO

---
+++ notes/src/main/java/com/example/notes/dtos/EmailDto.java
@@ -0,0 +1,19 @@
+package main.java.com.example.notes.dtos;
+
+import lombok.Data;
+
+@Data
+public class EmailDto {
+    @NotBlank
+    private String ownerRef;
+    @NotBlank
+    @Email
+    private String emailFrom;
+    @NotBlank
+    @Email
+    private String emailTo;
+    @NotBlank
+    private String subject;
+    @NotBlank
+    private String text;
+}
