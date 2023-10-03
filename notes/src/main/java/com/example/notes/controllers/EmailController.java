5130034c12d2a14b763165fec36a9800a8fcf3f7
Mikaéla <mikaela.cardoso@fatec.sp.gov.br>
2023-10-03 17:25:05 -0300

    [chore]: imports na EmailController

--- notes/src/main/java/com/example/notes/controllers/EmailController.java
+++ notes/src/main/java/com/example/notes/controllers/EmailController.java
@@ -1,8 +1,18 @@
-package main.java.com.example.notes.controllers;
+package com.example.notes.controllers;
 
-import main.java.com.example.notes.domain.Email;
-import main.java.com.example.notes.dtos.EmailDto;
-import main.java.com.example.notes.services.EmailService;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.notes.domain.Email;
+import com.example.notes.dtos.EmailDto;
+import com.example.notes.services.EmailService;
+
+import jakarta.validation.Valid;
 
 @RestController
 public class EmailController {
@@ -15,7 +25,7 @@ public class EmailController {
         Email email = new Email();
         BeanUtils.copyProperties(emailDto, email);
         emailService.sendEmail(email);
-        return new ResponseEntity<>(emailModel, HttpStatus.CREATED);
+        return new ResponseEntity<>(email, HttpStatus.CREATED);
     }
 
 }
